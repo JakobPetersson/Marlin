e93dbfd662d238712144da8a0e5ca9fbf1961542
Jakob Petersson <github@jakobpetersson.se>
2021-11-06 01:06:50 +0100

    Config: Change bed-size to 1000x900

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1577,8 +1577,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 1000
+#define Y_BED_SIZE 900
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
