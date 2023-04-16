f5d9895ab09357b5b673695d579453975d120c1c
Jakob Petersson <github@jakobpetersson.se>
2023-04-16 14:01:15 +0200

    Homing: Configure X- and Y-Axis homing position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1702,8 +1702,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS X_BED_SIZE + 17
+#define Y_MAX_POS Y_BED_SIZE + 7
 #define Z_MAX_POS 300
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
